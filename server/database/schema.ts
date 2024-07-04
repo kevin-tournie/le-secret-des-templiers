3b3d34b5525d5c580ddb33ea8143610e2805aa5a
ktournie <kevin.tournie@ifolia.fr>
2024-07-04 22:31:09 +0200

    firt commit + migration

---
+++ server/database/schema.ts
@@ -0,0 +1,15 @@
+import { sql } from "drizzle-orm";
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+
+export const teams = sqliteTable('teams', {
+    id: text('id'),
+    name: text('name').notNull(),
+    company: text('company').notNull(),
+    start_time: text('start_time'),
+    end_time: text('end_time'),
+    score: integer('score').notNull().default(0),
+    malus_points: integer('malus_points').notNull().default(0),
+
+    createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
+    updatedAt: text('updated_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
+});
