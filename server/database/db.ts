9b432aa840e22b6841bd1cbe0545b35541ebe789
kevin.tournie <kevin.tournie@capgemini.com>
2024-10-19 19:12:31 +0200

    feat: migrations in runtime

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,5 +1,16 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
 const { databaseUrl } = useRuntimeConfig();
 
 export const db = drizzle(databaseUrl);
+
+async function runMigrations() {
+    await migrate(db, {
+        migrationsFolder: './drizzle'
+    });
+
+    process.exit(0);
+}
+
+runMigrations();
